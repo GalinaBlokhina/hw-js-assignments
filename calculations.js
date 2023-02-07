a86c28ef59139cbff5bf5142dd96fdbff837e9ba
BlokhinaGalina <galina-a@yandex.ru>
2023-02-07 16:15:52 +0300

    Updated Calculatoins

--- calculations.js
+++ calculations.js
@@ -1,19 +1,19 @@
 function increaseBalance(x, y) {
-  return x + y
+  return x += y
 }
 console.log(increaseBalance(3000, 700))
 
 function decreaseBalance(x, y) {
-  return x-y
+  return x -= y
   }
   console.log(decreaseBalance(3000, 700))
 
 function divideBalanceByAccounts(x, y) {
-return x / y
+return x /= y
 }
 console.log(divideBalanceByAccounts(3000, 2, 4000))
 
 function getRestAfterDivision(x, y) {
-  return x % y
+  return x = x %= y
 }
 console.log(getRestAfterDivision(7000, 3))
